7308ecbb451641afcccc5a7643b89dcb0396c930
Guilhermee <xpgui@outlook.com>
2023-07-07 20:16:44 -0300

    Game over

--- js/validacoes.js
+++ js/validacoes.js
@@ -2,7 +2,23 @@ function verificaValor(chute) {
     const numero = +chute;
 
     if (Number.isNaN(numero)){
-        elementoChute.innerHTML += "<div>Valor Inválido</div>";
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML= `
+            <h2>Game Over!!</h2>
+            <h3>Pressione o botão abaixo para jogar novamente</h3>
+            
+            <button class="btn-jogar" id="jogar-novamente">Jogar Novamente</button>
+            `;
+        } else if (chute.toUpperCase() === "NINO") {
+            document.body.innerHTML= `
+            <h2>Conhece ele??</h2>
+            <h3>da onde tirou esse nome meu amigo?</h3>
+            
+            <button class="btn-jogar" id="jogar-novamente">Jogar Novamente</button>
+            `;
+        }else {
+            elementoChute.innerHTML += "<div>Valor Inválido</div>";
+        }
     }
 
     if (numero > maiorValor || numero < menorValor) {
